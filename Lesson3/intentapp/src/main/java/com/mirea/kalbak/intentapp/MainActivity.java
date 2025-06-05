f9babebfc22b476cbfdac8f91366c86c00ab86ab
ulgosipoy <dkalbak@mail.ru>
2025-06-05 22:27:19 +0300

    done task 1.5 and create module Sharer

---
+++ Lesson3/intentapp/src/main/java/com/mirea/kalbak/intentapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.mirea.kalbak.intentapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        long dateInMillis = System.currentTimeMillis();
+        String format = "yyyy-MM-dd HH:mm:ss";
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        String dateString = sdf.format(new Date(dateInMillis));
+
+        TextView tvTime = findViewById(R.id.tvTime);
+        tvTime.setText("Текущее время: " + dateString);
+
+        Button btnSend = findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            intent.putExtra("TIME", dateString);
+            startActivity(intent);
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ Lesson3/intentapp/src/main/java/com/mirea/kalbak/intentapp/SecondActivity.java
@@ -0,0 +1,36 @@
+package com.mirea.kalbak.intentapp;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+
+        String time = getIntent().getStringExtra("TIME");
+        int Number = 15;
+        int squared = Number * Number;
+
+        String result = "Квадрат значения моего номера по списку в группе составляет " +
+                squared + ", а текущее время " + time;
+
+        TextView tvResult = findViewById(R.id.tvResult);
+        tvResult.setText(result);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
