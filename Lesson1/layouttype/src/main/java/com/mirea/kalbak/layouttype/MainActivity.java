b6bf6bdbb1f525ee19f769fb1018306f6bf5ad2a
ulgosipoy <dkalbak@mail.ru>
2025-06-05 13:56:49 +0300

    done task from chapter 6 (in module layouttype)

--- Lesson1/layouttype/src/main/java/com/mirea/kalbak/layouttype/MainActivity.java
+++ Lesson1/layouttype/src/main/java/com/mirea/kalbak/layouttype/MainActivity.java
@@ -1,6 +1,9 @@
 package com.mirea.kalbak.layouttype;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
+        TextView myTextView = (TextView) findViewById(R.id.textViewStudent);
+        myTextView.setText("New text in MIREA");
+
+        Button buttonStudent = findViewById(R.id.buttonStudent);
+        buttonStudent.setText("MireaButton");
+
+        CheckBox checkBoxStudent = findViewById(R.id.checkBoxStudent);
+        checkBoxStudent.setChecked(true);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
